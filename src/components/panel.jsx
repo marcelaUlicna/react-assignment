fe4b338ceb38e2b9981a5c0b7f71bdfef7a6ce5c
Marcela Ulicna <marcelaUlicna@users.noreply.github.com>
2018-07-13 14:59:13 +0200

    Draft panel component

---
+++ src/components/panel.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class Panel extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>AssignmentComponent</p>
+                <p>TaskAssignmentComponent</p>
+            </div>
+        );
+    }
+}
